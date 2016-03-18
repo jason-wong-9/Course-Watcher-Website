ba1da315c51ba6fa2f0742010fbf138863cf7308
Jason Wong <jason_19960903@hotmail.com>
2016-03-17 18:32:45 -0700

    make it better

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	"database": "mongodb://root:abc123@ds023078.mlab.com:23078/course-watcher",
+	"dev": true,
+	"secretKey": "YourSecretKey",
+	"gmailUser": "acidrainsavers2@gmail.com",
+	"gmailPass": "1a2s3d4f5"
+}
