213ef0ad8aab5b95b139cbd8cf971976563cd949
Jason Wong <j.wong.9@alumni.ubc.ca>
2016-03-17 18:39:29 -0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	"database": "mongodb://root:abc123@ds023078.mlab.com:23078/course-watcher",
-	"dev": true,
-	"secretKey": "YourSecretKey",
-	"gmailUser": "acidrainsavers2@gmail.com",
-	"gmailPass": "1a2s3d4f5"
-}
